8e2f45e631dae2c68b5a8557b8c2161535c84d83
Dvd2112 <david.junior211204@gmail.com>
2025-04-24 20:17:24 -0300

    estudos java aula 2

--- Programa1.java
+++ Programa1.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 public class Programa1{
     public static void main (String[] args){
-        String nome;
-        int idade;
-        double salario;
-        
+        Scanner scanner = new Scanner(System.in);
+
+        String nome = "fulano";
+        double salario = 1023.27;
+        boolean ativo = true;
+        char sexo = 'm';
+        System.out.println(nome);
+        System.out.println(salario);
+        System.out.println(ativo);
+        System.out.println(sexo);
+        System.out.println("informe sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("sua idade"+ idade);
     }
 }
---
+++ Programa2.java
@@ -0,0 +1,15 @@
+public class Programa2 {
+    public static void main (String[] args){
+        String nome = "fulano";
+        int idade = 19;
+        double peso = 75.57;
+        boolean matriculado = true;
+        char situacao = 'c';
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(peso);
+        System.out.println(matriculado);
+        System.out.println(situacao);
+        
+    }
+}
